f253f09d16b9c451136f7e39fd2d51fd3d83222b
reimondo27 <80732735+reimondo27@users.noreply.github.com>
2021-06-15 20:17:28 +0700

    Priority List

---
+++ scriptprio.js
@@ -0,0 +1,57 @@
+
+function startClock() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(startClock, 1000);
+  }
+
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+
+  $(".txtb").on("keyup",function(e){
+    //13  means enter button
+    if(e.keyCode == 13 && $(".txtb").val() != "")
+    {
+      var task = $("<div class='task'></div>").text($(".txtb").val());
+      var del = $("<i class='fas fa-trash-alt'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          p.remove();
+        });
+      });
+
+      var check = $("<i class='fas fa-check'></i>").click(function(){
+        var p = $(this).parent();
+        p.fadeOut(function(){
+          $(".comp").append(p);
+          p.fadeIn();
+        });
+        $(this).remove();
+      });
+
+      task.append(del,check);
+
+      var ddl = ($(".priorityLevel").val());
+      switch (ddl){
+        case "High" :
+          $(".HighPrio").append(task); break;
+
+        case "Medium" :
+          $(".MedPrio").append(task); break;
+
+        case "Low" :
+          $(".LowPrio").append(task); break;
+      }
+        //to clear the input
+      $(".txtb").val("");
+    }
+  });
+
+  startClock();
