da9cd0c59d979cb6155e688df413cd2b72ba198e
Uto <daffamadeo@gmail.com>
2021-06-16 18:11:34 +0700

    fixed timer function

--- src/timerFunction.js
+++ src/timerFunction.js
@@ -7,15 +7,13 @@ var s = document.getElementById("sec");
 
 //store a reference to the startTimer variable
 var startTimer = null;
+var hasStarted = false;
 
 start.addEventListener("click", function () {
-  //initialize the variable
-  function startInterval() {
-    startTimer = setInterval(function () {
-      timer();
-    }, 1000);
-  }
-  startInterval();
+    if(!hasStarted){
+      startInterval();
+      hasStarted = true;
+    }
 });
 
 reset.addEventListener("click", function () {
@@ -26,25 +24,62 @@ reset.addEventListener("click", function () {
   stopInterval();
 });
 
+function startInterval() {
+  startTimer = setInterval(function () {
+    timer();
+  }, 1000);
+}
+
 function timer() {
+  var moduloTemp;
+  if (s.value > 59) {
+    moduloTemp = s.value % 60;
+    console.log(moduloTemp);
+    m.value += (s.value - moduloTemp) / 60;
+    s.value = moduloTemp;
+  }
+
+  if (m.value > 59) {
+    moduloTemp = m.value % 60;
+    console.log(moduloTemp);
+    h.value += (m.value - moduloTemp) / 60;
+    m.value = moduloTemp;
+  }
+
   if (h.value == 0 && m.value == 0 && s.value == 0) {
     h.value = 0;
     m.value = 0;
     s.value = 0;
-  } else if (s.value != 0) {
+  } 
+  
+  else if (s.value != 0) {
     s.value--;
-  } else if (m.value != 0 && s.value == 0) {
+  } 
+  
+  else if (m.value != 0 && s.value == 0) {
     s.value = 59;
     m.value--;
-  } else if (h.value != 0 && m.value == 0) {
+  } 
+  
+  else if (h.value != 0 && m.value == 0) {
     m.value = 60;
     h.value--;
   }
   return;
 }
 
+function checkTime() {
+  var extraM = 0;
+  if (s.value > 60) {
+    extraH = s.value % 60;
+    s.value = 60;
+    m.value = extraM;
+  }
+}
+
 //stop the function after pressing the reset button,
 //so the time wont go down when selecting a new time after pressing reset
 function stopInterval() {
   clearInterval(startTimer);
+  hasStarted = false;
 }
