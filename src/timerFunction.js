60d47f92a524c52e932f2c32fe34880eae2a4595
Uto <daffamadeo@gmail.com>
2021-06-16 18:52:08 +0700

    ui design for timer

--- src/timerFunction.js
+++ src/timerFunction.js
@@ -1,26 +1,24 @@
-var start = document.getElementById("start");
-var reset = document.getElementById("reset");
+var start = document.getElementById("startBtn");
+var reset = document.getElementById("resetBtn");
 
-var h = document.getElementById("hour");
-var m = document.getElementById("minute");
-var s = document.getElementById("sec");
+var hour = document.getElementById("hour");
+var min = document.getElementById("minute");
+var sec = document.getElementById("sec");
 
-//store a reference to the startTimer variable
 var startTimer = null;
 var hasStarted = false;
 
 start.addEventListener("click", function () {
-    if(!hasStarted){
+    if(!hasStarted && (hour.value == 0 && min.value == 0 && sec.value == 0)){
       startInterval();
       hasStarted = true;
     }
 });
 
 reset.addEventListener("click", function () {
-  h.value = 0;
-  m.value = 0;
-  s.value = 0;
-  //stop the timer after pressing "reset"
+  hour.value = 0;
+  min.value = 0;
+  sec.value = 0;
   stopInterval();
 });
 
@@ -32,53 +30,43 @@ function startInterval() {
 
 function timer() {
   var moduloTemp;
-  if (s.value > 59) {
-    moduloTemp = s.value % 60;
+  if (sec.value > 59) {
+    moduloTemp = sec.value % 60;
     console.log(moduloTemp);
-    m.value += (s.value - moduloTemp) / 60;
-    s.value = moduloTemp;
+    min.value += (sec.value - moduloTemp) / 60;
+    sec.value = moduloTemp;
   }
 
-  if (m.value > 59) {
-    moduloTemp = m.value % 60;
+  if (min.value > 59) {
+    moduloTemp = min.value % 60;
     console.log(moduloTemp);
-    h.value += (m.value - moduloTemp) / 60;
-    m.value = moduloTemp;
+    hour.value += (min.value - moduloTemp) / 60;
+    min.value = moduloTemp;
   }
 
-  if (h.value == 0 && m.value == 0 && s.value == 0) {
-    h.value = 0;
-    m.value = 0;
-    s.value = 0;
+  if (hour.value == 0 && min.value == 0 && sec.value == 0) {
+    hour.value = 0;
+    min.value = 0;
+    sec.value = 0;
+    //MAININ ALARM DISINI
   } 
   
-  else if (s.value != 0) {
-    s.value--;
+  else if (sec.value != 0) {
+    sec.value--;
   } 
   
-  else if (m.value != 0 && s.value == 0) {
-    s.value = 59;
-    m.value--;
+  else if (min.value != 0 && sec.value == 0) {
+    sec.value = 59;
+    min.value--;
   } 
   
-  else if (h.value != 0 && m.value == 0) {
-    m.value = 60;
-    h.value--;
+  else if (hour.value != 0 && min.value == 0) {
+    min.value = 60;
+    hour.value--;
   }
   return;
 }
 
-function checkTime() {
-  var extraM = 0;
-  if (s.value > 60) {
-    extraH = s.value % 60;
-    s.value = 60;
-    m.value = extraM;
-  }
-}
-
-//stop the function after pressing the reset button,
-//so the time wont go down when selecting a new time after pressing reset
 function stopInterval() {
   clearInterval(startTimer);
   hasStarted = false;
